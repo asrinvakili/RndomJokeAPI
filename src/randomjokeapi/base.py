957d0ad217deb2af9e2da8080c22675d29e30fe6
asrinvakili <34458009+asrinvakili@users.noreply.github.com>
2024-07-09 17:46:40 +0330

    setup

--- src/randomjokeapi/base.py
+++ src/randomjokeapi/base.py
@@ -2,8 +2,6 @@ from abc import ABC, abstractmethod
 
 
 class Joke(ABC):
-    def __init__(self, **kwargs):
-        pass
 
     @abstractmethod
     def get_random_joke(self):
---
+++ test.py
@@ -0,0 +1,16 @@
+from randomjokeapi.api import A, B, C
+
+# استفاده از کلاس A
+joke_a = A()
+print("Joke from A:")
+print(joke_a.get_random_joke())
+
+# استفاده از کلاس B
+joke_b = B()
+print("\nJoke from B:")
+print(joke_b.get_random_joke())
+
+# استفاده از کلاس C
+joke_c = C()
+print("\nJoke from C:")
+print(joke_c.get_random_joke())
